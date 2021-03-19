3754c5c2552f033d0d29441419745dd39fb20baf
Dominik Schidlowski <dominik@schidlowski.eu>
2021-03-19 14:08:01 +0100

    check file contents in tests

--- decoder/decoder_test.go
+++ decoder/decoder_test.go
@@ -3,6 +3,7 @@ package decoder
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"os"
 	"path"
 	"testing"
@@ -19,21 +20,29 @@ func TestRun(t *testing.T) {
 	err := Run(options)
 	assert.NoError(t, err)
 
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/library/meta.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/slot6/meta.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/system/meta.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/unit/meta.json"))
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/library/meta.json"), "{\"slotKey\":\"-3\",\"slot\":{\"name\":\"library\",\"type\":{\"events\":[],\"methods\":[]}}}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/slot6/meta.json"), "{\"slotKey\":\"0\",\"slot\":{\"name\":\"slot6\",\"type\":{\"events\":[],\"methods\":[]}}}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/system/meta.json"), "{\"slotKey\":\"-2\",\"slot\":{\"name\":\"system\",\"type\":{\"events\":[],\"methods\":[]}}}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/unit/meta.json"), "{\"slotKey\":\"-1\",\"slot\":{\"name\":\"unit\",\"type\":{\"events\":[],\"methods\":[]}}}")
 
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/slot6/handler/0.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/slot6/handler/0.lua"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/system/handler/3.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/system/handler/3.lua"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/system/handler/4.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/system/handler/4.lua"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/system/handler/5.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/system/handler/5.lua"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/unit/handler/1.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/unit/handler/1.lua"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/unit/handler/2.json"))
-	assert.FileExists(t, path.Join(options.OutputDirectory, "slot/unit/handler/2.lua"))
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/slot6/handler/0_start()/meta.json"), "{\"code\":\"\",\"filter\":{\"args\":[],\"signature\":\"start()\",\"slotKey\":\"0\"},\"key\":\"0\"}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/slot6/handler/0_start()/code.lua"), "code0")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/system/handler/3_actionStart(action)/meta.json"), "{\"code\":\"\",\"filter\":{\"args\":[{\"value\":\"option2\"}],\"signature\":\"actionStart(action)\",\"slotKey\":\"-2\"},\"key\":\"3\"}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/system/handler/3_actionStart(action)/code.lua"), "code3")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/system/handler/4_actionStart(action)/meta.json"), "{\"code\":\"\",\"filter\":{\"args\":[{\"value\":\"option1\"}],\"signature\":\"actionStart(action)\",\"slotKey\":\"-2\"},\"key\":\"4\"}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/system/handler/4_actionStart(action)/code.lua"), "code4")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/system/handler/5_actionStart(action)/meta.json"), "{\"code\":\"\",\"filter\":{\"args\":[{\"value\":\"option3\"}],\"signature\":\"actionStart(action)\",\"slotKey\":\"-2\"},\"key\":\"5\"}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/system/handler/5_actionStart(action)/code.lua"), "code5")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/unit/handler/1_stop()/meta.json"), "{\"code\":\"\",\"filter\":{\"args\":[],\"signature\":\"stop()\",\"slotKey\":\"-1\"},\"key\":\"1\"}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/unit/handler/1_stop()/code.lua"), "code1")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/unit/handler/2_tick(timerId)/meta.json"), "{\"code\":\"\",\"filter\":{\"args\":[{\"value\":\"Live\"}],\"signature\":\"tick(timerId)\",\"slotKey\":\"-1\"},\"key\":\"2\"}")
+	assertFileContent(t, path.Join(options.OutputDirectory, "slot/unit/handler/2_tick(timerId)/code.lua"), "code2")
+
+}
+
+func assertFileContent(t *testing.T, path string, content string) {
+	assert.FileExists(t, path)
+	buffer, err := ioutil.ReadFile(path)
+	assert.NoError(t, err)
+	assert.Equal(t, content, string(buffer))
 }
